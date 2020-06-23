48970dec5acc702a50675faae48259760a746e53
Ravikirandhss <dhss.ravikiran@gmail.com>
2020-06-23 21:19:39 +0530

    Updates

---
+++ precache-manifest.f43dbe82876f58cad2726bab03776195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1708faad7adcfefe75d79f12a6cca7",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "1bb12afe63e6eec3a242",
+    "url": "/react-gh-pages/static/css/main.4d26ffb0.chunk.css"
+  },
+  {
+    "revision": "0e82309d75c49f989549",
+    "url": "/react-gh-pages/static/js/2.1d9c8898.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.1d9c8898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb12afe63e6eec3a242",
+    "url": "/react-gh-pages/static/js/main.653bc9ca.chunk.js"
+  },
+  {
+    "revision": "c910589a90c7db292e97",
+    "url": "/react-gh-pages/static/js/runtime-main.8d2d02b4.js"
+  },
+  {
+    "revision": "cb0caa49c4e5330f47af75295fe5d031",
+    "url": "/react-gh-pages/static/media/icon.cb0caa49.jpeg"
+  }
+]);
